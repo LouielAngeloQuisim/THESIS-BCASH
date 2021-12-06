9f31c45266c7af93e276b8c78b266d148eb6bcbd
arvinpoblete <poblete.deguzmanarvinjay@gmail.com>
2021-12-06 15:35:49 +0800

    user side js

--- dashboard.php
+++ dashboard.php
@@ -33,6 +33,8 @@ else{
 <!doctype html>
 <html lang="en">
   <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.js"></script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
