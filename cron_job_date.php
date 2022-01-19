8847c67172714af37abd870f70160b4db8b9ea96
LouielAngeloQuisim <60286235+LouielAngeloQuisim@users.noreply.github.com>
2022-01-19 20:17:17 +0800

    remoe echo from cron date

--- cron_job_date.php
+++ cron_job_date.php
@@ -7,10 +7,8 @@ $total_bottle = $mydb->get_dailyRecycle($date);
 $total_redeem = $mydb->get_dailyRedeem($date);
 if($find_date == "true"){
     $result = $mydb->update_Dailyreport($date, $total_bottle, $total_redeem);
-    echo $result;
 }
 else{
     $result = $mydb->add_Dailyreport($date, $total_bottle, $total_redeem);
-    echo $result;
 }
 
