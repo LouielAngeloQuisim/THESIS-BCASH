c6f342e6c39a80b86c50c567552045c64a8de4a7
arvinpoblete <poblete.deguzmanarvinjay@gmail.com>
2022-03-18 21:43:15 +0800

    chuchu

---
+++ select.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="sass/main.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>Login</title>
+  </head>
+  <body>
+      <section class="bg-white p-5">
+      </section>
+      <section class="bg-white p-5">
+      </section>
+      <section class="bg-white p-5">
+        <img src="img/logo1.png" class="mx-auto d-block mb-3">
+        <div class="d-grid gap-3 col-lg-4 mx-auto">
+            <a class="btn btn-primary btn-lg rounded-pill" href="login.php" role="button">Login</a>
+            <a class="btn btn-primary btn-lg rounded-pill" href="dash_shop.php" role="button">Shop</a>
+        </div>
+      </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+  </body>
+</html>
