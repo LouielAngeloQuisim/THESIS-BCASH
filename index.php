fdb7f21ec91790e481cddd9b2ab6dae73ccbb29f
arvinpoblete <poblete.deguzmanarvinjay@gmail.com>
2022-03-29 21:17:26 +0800

    all rights chuchu

--- index.php
+++ index.php
@@ -23,9 +23,15 @@
             <a class="btn btn-primary btn-lg rounded-pill" href="dash_shop.php" role="button">Shop</a>
           </div>
         </div>
-        
-        
       </section>
+
+      <div class="container text-center">
+        <div class="fixed-bottom bg-primary sniff">
+          <p class="fs-6 text-white pp">
+            All Rights Reserved 2022
+          </p>
+        </div>
+      </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
