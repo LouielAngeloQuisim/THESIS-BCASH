04343f4d512240b55c4d508d71236947b79e9d53
arvinpoblete <poblete.deguzmanarvinjay@gmail.com>
2022-04-07 22:50:35 +0800

    recycle into redeem

--- transac_admin_redeem.php
+++ transac_admin_redeem.php
@@ -231,7 +231,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalgenRepRedeem">Generate Recycle Reports</h5>
+                    <h5 class="modal-title" id="modalgenRepRedeem">Generate Redeem Reports</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
                 <div class="modal-body">
